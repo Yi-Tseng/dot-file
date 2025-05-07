a027148c99632d9b9e0f85f7ea20b0fe30a6c343
Tseng, Yi <yi.tseng@intel.com>
2025-05-07 15:28:50 -0700

    update installer

--- install.sh
+++ install.sh
@@ -19,13 +19,14 @@ sudo dpkg -i /tmp/k9s.deb
 rm -f /tmp/k9s.deb
 
 # Install krew
-set -x; cd "$(mktemp -d)"
+pushd "$(mktemp -d)"
 OS="$(uname | tr '[:upper:]' '[:lower:]')"
 ARCH="$(uname -m | sed -e 's/x86_64/amd64/' -e 's/\(arm\)\(64\)\?.*/\1\2/' -e 's/aarch64$/arm64/')"
 KREW="krew-${OS}_${ARCH}"
 curl -fsSLO "https://github.com/kubernetes-sigs/krew/releases/latest/download/${KREW}.tar.gz"
 tar zxvf "${KREW}.tar.gz"
 ./"${KREW}" install krew
+popd
 
 # Copy dotfiles
 cp bashrc ~/.bashrc
